d84fea1ac4f9f3a84acc5359aaa8374d1851921e
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-07-18 00:35:45 -0300

    Project created

---
+++ backend/src/main/java/com/stepinfo/dsmeta/DSMetaMeu/DsMetaMeuApplication.java
@@ -0,0 +1,13 @@
+package com.stepinfo.dsmeta.DSMetaMeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsMetaMeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsMetaMeuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/stepinfo/dsmeta/DSMetaMeu/DsMetaMeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepinfo.dsmeta.DSMetaMeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMetaMeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
